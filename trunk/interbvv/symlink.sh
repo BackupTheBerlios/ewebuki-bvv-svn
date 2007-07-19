a5d73a3f40c3e127f663540a3e7fca7a844fefa8
chaot <chaot@9b64456f-e72c-0410-83d5-e13b72aa00c8>
2007-07-19 11:06:32 +0000

    script fuer die symlinks

---
+++ trunk/interbvv/symlink.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+ln -s css/admin.css ../interbvv/css/
+ln -s css/col0.css ../interbvv/css/
+ln -s css/col1.css ../interbvv/css/
+ln -s css/col2.css ../interbvv/css/
+ln -s css/col3.css ../interbvv/css/
+ln -s css/col4.css ../interbvv/css/
+ln -s css/col5.css ../interbvv/css/
+ln -s css/studie.css ../interbvv/css/
+ln -s css/html.css ../interbvv/css/
+
+
+ln -s images/html ../interbvv/images/
+ln -s images/studie ../interbvv/images/
+
+ln -s modules/customer ../interbvv/modules/
+
+ln -s templates/html ../interbvv/templates/
+ln -s templates/studie ../interbvv/templates/
