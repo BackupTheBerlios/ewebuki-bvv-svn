2dfac2ae980e9bb4471b1e568e56ebc9c7b9c151
buffy1860 <buffy1860@9b64456f-e72c-0410-83d5-e13b72aa00c8>
2009-02-17 16:00:01 +0000

    erweiterte suche

--- trunk/interbvv/modules/customer/suche.inc.php
+++ trunk/interbvv/modules/customer/suche.inc.php
@@ -42,8 +42,44 @@
     URL: http://www.chaos.de
 */
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    // strong,code,a
+
     if ( $debugging["html_enable"] ) $debugging["ausgabe"] .= "[ ** ".$script["name"]." ** ]".$debugging["char"];
+
+    // steuerung der reiter
+    $ausgaben["search_sel"] = "selected";
+    $ausgaben["esearch_sel"] = "";
+    $hidedata["search"]["on"] = "on";
+    $ausgaben["aktion"] = "suche.html";
+    $ausgaben["displaysite"] = "";
+    $ausgaben["displayfile"] = " display:none";
+    $ausgaben["datresult"] = " display:none";
+
+    if ( $_POST["fpage"] ) {
+        $ausgaben["displaysite"] = " display:none";
+        $ausgaben["displayfile"] = "";
+    }
+
+    if ( !$_POST["words"] ) {
+        $ausgaben["displaysite"] = " display:none";
+        $ausgaben["displayfile"] = " display:none";
+    }
+
+
+    if ( $environment["parameter"][1] ) {
+        $ausgaben["aktion"] = "suche,esearch.html";
+        $ausgaben["search_sel"] = "";
+        $hidedata["esearch"]["on"] = "";
+        $ausgaben["esearch_sel"] = "selected";
+        unset($hidedata["search"]);
+        // restrict dropdown bauen
+        $sql_restrict = "SELECT * FROM site_menu INNER JOIN site_menu_lang ON (site_menu.mid=site_menu_lang.mlid)WHERE refid='0' AND ( hide is Null or hide = '0' ) ORDER by sort";
+        $result_restrict = $db -> query($sql_restrict);
+        while ( $data = $db -> fetch_array($result_restrict) ){
+            $dataloop["restrict"][$data["entry"]]["entry"] = $data["entry"];
+            $dataloop["restrict"][$data["entry"]]["label"] = $data["label"];
+        }
+
+    }
 
     $ausgaben["suchbegriff"] = "";
     if ( $_POST["matchesperpage"] != "" ) {
@@ -62,6 +98,10 @@
     }
 
     $suchanfrage = urlencode(utf8_decode($_POST["words"]));
+
+    $suchanfrage2 = preg_replace("/%F6/","oe",$suchanfrage);
+
+
     if ( $suchanfrage != "" ) $ausgaben["suchbegriff"] = $_POST["words"];
     if ( $_POST["matchesperpage"] != "" ) {
         $matchesperpage = "&matchesperpage=".$_POST["matchesperpage"];
@@ -72,12 +112,12 @@
     }
 
     $page = "";
-    if ( $_POST["page"] != "" ) {
-        $page_org = $_POST["page"]+1;
+    if ( $_POST["spage"] != "" || $_POST["fpage"] != "" ) {
+        $page_org = $_POST["spage"]+1;
+        if ( $_POST["fpage"] != "" ) $page_org = $_POST["fpage"]+1;
         $page = "&page=".$page_org;
     }
-
-    $fp=fopen("http://".$network_adress."/cgi-bin/htsearch?words=".$suchanfrage."&config=".$cfg["suche"]["config"].$matchesperpage.$page,"r");
+    $fp=fopen("http://".$network_adress."/cgi-bin/htsearch?words=".$suchanfrage."&restrict=".$_POST["restrict"]."&exclude=http://devel2/file/&method=and&config=".$cfg["suche"]["config"].$matchesperpage.$page,"r");
 
     while ( $line = fgets($fp,1000) ){
         $line = preg_replace("/<a href=\"[A-Za-z0-9#-_:\/\"\.]*>/U","",$line);
@@ -90,6 +130,25 @@
             $dataloop["treffer"][] = explode("##",$line);
         }
     }
+
+    $i = 0;
+    $fp1=fopen("http://".$network_adress."/cgi-bin/htsearch?words=".$suchanfrage2."&restrict=http://devel2/file/&".$_POST["restrict"]."&method=any&config=".$cfg["suche"]["config"].$matchesperpage.$page,"r");
+    while ( $line = fgets($fp1,1000) ){
+        $i++;
+        $line = preg_replace("/<a href=\"[A-Za-z0-9#-_:\/\"\.]*>/U","",$line);
+        $line = str_replace("</a>","",$line);
+        if ( preg_match("/^http:\/\/(.*)/",$line,$match) ) {
+            if ( $cfg["suche"]["alien_index"][$fqdn][0] != ""  ) {
+                $fqdn = $cfg["suche"]["alien_index"][$fqdn][0];
+            }
+
+            $line = preg_replace("/^http:\/\/".substr($match[1],0,strpos($match[1],"/"))."/","http://".$fqdn.$pathvars["virtual"],$line);$pathvars["virtual"];
+            $dataloop["files_treffer"][] = explode("##",$line);
+        }
+    }
+
+    if ( $i > 0 && $environment["parameter"][1] == "") $ausgaben["datresult"] = "";
+
     if ( $page_org == 0 ) $page_org = 1;
     $page_org = $page_org-1;
 
@@ -111,6 +170,24 @@
         $ausgaben["result"] = "Ergebnis ".$begin." bis ".$end." von ".$dataloop["treffer"][0][5];
     }
 
+    // anzeige der trefferanzahl
+    $site_count_files = floor($dataloop["files_treffer"][0][5] / $hits_per_site);
+    if ( !$dataloop["files_treffer"][0][5] ) {
+        $ausgaben["filesresult"] = "Keine Treffer f&uuml;r: ".$suchanfrage;
+    } elseif ( $dataloop["files_treffer"][0][5] == 1 ) {
+        $ausgaben["filesresult"] = "Ein Treffer";
+    } else {
+        $begin = $page_org*$hits_per_site+1;
+        if ( $site_count > 0 ) {
+            $end = $page_org*$hits_per_site+$hits_per_site;
+            if ( $end > $dataloop["files_treffer"][0][5] ) $end = $dataloop["files_treffer"][0][5];
+        } else {
+            $end = $dataloop["files_treffer"][0][5];
+        }
+
+        $ausgaben["filesresult"] = "Ergebnis ".$begin." bis ".$end." von ".$dataloop["files_treffer"][0][5];
+    }
+
     // buffy-umschalter
     if ( $site_count > 0 ) {
         $dataloop["site_switch"][0]["site"] = 0;
@@ -122,6 +199,19 @@
             $dataloop["site_switch"][$counter]["site"] = $counter;
         }
         $dataloop["site_switch"][0]["site"] = 0;
+    }
+
+    // buffy-umschalter
+    if ( $site_count_files > 0 ) {
+        $dataloop["files_site_switch"][0]["site"] = 0;
+        $counter = 0;
+        while ( $counter < $site_count_files ) {
+           // bei 20 seiten ist schluss
+            if ( $counter == 20 ) break;
+            $counter++;
+            $dataloop["files_site_switch"][$counter]["site"] = $counter;
+        }
+        $dataloop["files_site_switch"][0]["site"] = 0;
     }
 
     // warnung ausgeben
