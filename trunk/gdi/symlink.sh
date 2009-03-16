e194c34990722e12c0d5252d7fcb51f022f9ad55
krompi <krompi@9b64456f-e72c-0410-83d5-e13b72aa00c8>
2009-03-16 09:16:22 +0000

    ordnerstruktur angelegt

---
+++ trunk/gdi/symlink.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd ../gdi/css/
+
+ln -s -f ../../gdi.ext/css/gdi
+
+cd ../images
+
+ln -s -f ../../gdi.ext/images/gdi
+
+cd ../js
+
+ln -s -f ../../gdi.ext/js/gdi
+
+cd ../modules
+
+ln -s -f ../../gdi.ext/modules/gdi
+
+cd ../templates
+
+ln -s -f ../../gdi.ext/templates/gdi
