579dc1fd0f110584d6bffac164c81191a88b0c34
krompi <krompi@9b64456f-e72c-0410-83d5-e13b72aa00c8>
2009-07-15 07:12:49 +0000

    bugfix

--- trunk/interbvv/modules/customer/amtsuche.inc.php
+++ trunk/interbvv/modules/customer/amtsuche.inc.php
@@ -100,7 +100,7 @@
             // amt-kennzahl
             $akz = $data[$cfg["amtsuche"]["db"]["plz"]["amt"]];
 
-            if ( date("U") < mktime(15,0,0,8,2,2009) || !strstr($_SERVER["SERVER_ADDR"],"10.248.65") || !strstr($_SERVER["SERVER_ADDR"],"10.192.101.47") ) {
+            if ( date("U") < mktime(15,0,0,7,2,2009) || !strstr($_SERVER["SERVER_ADDR"],"10.248.65") || !strstr($_SERVER["SERVER_ADDR"],"10.192.101.47") ) {
                 header("Location: ".$pathvars["virtual"]."/aemter/".$akz."/index.html");
                 exit;
             }
