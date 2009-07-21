886c51480abae8e5d4fb82b1551bf5ca2eea1e62
krompi <krompi@9b64456f-e72c-0410-83d5-e13b72aa00c8>
2009-07-21 09:14:37 +0000

    noch ein versuch

--- trunk/interbvv/js/html/fontsize.js
+++ trunk/interbvv/js/html/fontsize.js
@@ -1,19 +1,22 @@
     function fontsize_change(size) {
-        normal  = 76+"%";
-        large   = 85+"%";
-        largest = 100+"%";
+        normal  = "1.0em";
+        large   = "1.17em";
+        largest = "1.33em";
         if ( size == 'xl' ) {
-            document.body.style.fontSize = largest;
+            $('site').style.fontSize = largest;
+            $('head').style.fontSize = largest;
             $('font_size1').removeClassName('font_sel');
             $('font_size2').removeClassName('font_sel');
             $('font_size3').addClassName('font_sel');
         } else if ( size == 'l' ) {
-            document.body.style.fontSize = large;
+            $('site').style.fontSize = large;
+            $('head').style.fontSize = large;
             $('font_size1').removeClassName('font_sel');
             $('font_size2').addClassName('font_sel');
             $('font_size3').removeClassName('font_sel');
         } else if ( size == 'n' ) {
-            document.body.style.fontSize = normal;
+            $('site').style.fontSize = normal;
+            $('head').style.fontSize = normal;
             $('font_size1').addClassName('font_sel');
             $('font_size2').removeClassName('font_sel');
             $('font_size3').removeClassName('font_sel');
