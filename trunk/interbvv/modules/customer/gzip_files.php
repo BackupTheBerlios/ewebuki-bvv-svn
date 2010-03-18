4d03d314cd03d72ba8b0d2248733f1dc89ca1d0e
krompi <krompi@9b64456f-e72c-0410-83d5-e13b72aa00c8>
2010-03-18 13:32:07 +0000

    beschleunigungsversuche

--- trunk/interbvv/modules/customer/gzip_files.php
+++ trunk/interbvv/modules/customer/gzip_files.php
@@ -1,28 +1,75 @@
 <?php
-    function my_obstart() {
-        $encode = getenv("HTTP_ACCEPT_ENCODING");
-        if(ereg("gzip",$encode)) {
-            ob_start("ob_gzhandler");
-        } else {
-            ob_start();
+
+// Include this function on your pages
+if ( !function_exists("print_gzipped_page") ) {
+    function print_gzipped_page() {
+
+        $HTTP_ACCEPT_ENCODING = getenv("HTTP_ACCEPT_ENCODING");
+        if( headers_sent() ){
+            $encoding = false;
+        }elseif( strpos($HTTP_ACCEPT_ENCODING, 'x-gzip') !== false ){
+            $encoding = 'x-gzip';
+        }elseif( strpos($HTTP_ACCEPT_ENCODING,'gzip') !== false ){
+            $encoding = 'gzip';
+        }else{
+            $encoding = false;
+        }
+
+        if( $encoding ){
+            $contents = ob_get_contents();
+            ob_end_clean();
+            header('Content-Encoding: '.$encoding);
+            header("ETag: ".md5($contents)); // ETag im Header senden
+            header("Expires: ".date("r",mktime(0,0,0,date("n"),date("j")+365)));
+            print("\x1f\x8b\x08\x00\x00\x00\x00\x00");
+            $size = strlen($contents);
+            $contents = gzcompress($contents, 9);
+            $contents = substr($contents, 0, $size);
+            print($contents);
+//             exit();
+        }else{
+            ob_end_flush();
+//             exit();
         }
     }
-    my_obstart(); // Führt die Funktion nun aus
+}
 
-    $path = dirname(dirname(__FILE__))."/";
-    $path = str_replace("/modules/","",$path);
-    $path = str_replace(".ext","",$path);
+// At the beginning of each page call these two functions
+ob_start();
+ob_implicit_flush(0);
 
-    $file = $path.$_SERVER["REQUEST_URI"];
-    $ext_array = array("css","js");
+// Then do everything you want to do on the page
+$path = dirname(dirname(__FILE__))."/";
+$path = str_replace("/modules/","",$path);
+$path = str_replace(".ext","",$path);
 
-    $extension = substr(basename($file),(strrpos(basename($file),".")+1));
+$file = $path.$_SERVER["REQUEST_URI"];
+$ext_array = array("css","js");
+
+$extension = substr(basename($file),(strrpos(basename($file),".")+1));
+
+if ( in_array($extension,$ext_array) ) {
+    header("Content-Type: text/".$extension);
+    echo readfile($file);
+} else {
+    exit;
+}
+
+// Call this function to output everything as gzipped content.
+print_gzipped_page();
 
 
-    if ( in_array($extension,$ext_array) ) {
-        header("Content-Type: text/".$extension);
-        readfile($file);
-    } else {
-        exit;
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 ?>
